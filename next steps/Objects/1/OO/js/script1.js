6ff4632c8a5ff854071ef3f7d4d00673b3fa5f99
Ezedcode <dinog165@gmail.com>
2022-07-28 11:21:09 +0000

    creating objects

---
+++ next steps/Objects/1/OO/js/script1.js
@@ -0,0 +1,21 @@
+class Product {
+
+    constructor(){
+        this.id = 0
+        this.nameProduct = ''
+        this.valor = 0
+    }
+
+    add() {
+        alert("Let's Add a product")
+        //...
+    }
+    
+    delete() {
+        //...
+
+        alert("Deleted item")
+    }
+}
+
+var product = new Product();
